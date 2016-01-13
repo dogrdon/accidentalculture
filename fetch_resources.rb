f421935a2e5f58cd6b7854841b58560da30b6ff1
drew gordon <drew.s.gordon@gmail.com>
2016-01-12 23:54:44 -0500

    make request builder a little smoother

--- fetch_resources.rb
+++ fetch_resources.rb
@@ -8,17 +8,14 @@ require 'rest-client'
 require 'nokogiri'
 require 'capybara/poltergeist'
 require 'optparse'
+require 'cgi'
 require 'json'
 require_relative 'config/api_keys'
 
 
 
 #set up for api, search
-__APIS__ = {"dpla"=>{"base"=>"http://api.dp.la/v2/", 
-					 "items"=>"items", 
-					 "params"=>{"api_key"=>API_KEYS['dpla']}
-					 } 
-		    }
+__APIS__ = {:dpla=>"http://api.dp.la/v2/%s?api_key=#{API_KEYS['dpla']}&%s"}
 
 class Client
 
@@ -30,13 +27,18 @@ class Client
 
 	end
 
+	def encode_params(params_hash)
+		params_hash.map{|k,v| CGI.escape(k) + '=' + CGI.escape(v)}.join('&')
+	end
+
 	def search(type, term, format)
-		@type = @service[type]
-		@params_hash = @service['params']
+		@type = type
+		@params_hash = Hash.new
 		@params_hash['q'] = term
 		@params_hash['sourceResource.type'] = format
-		url = @service['base'] + @type 
-		res = RestClient.get url, {:params => @params_hash}
+		@params = encode_params @params_hash
+		url = @service % [@type, @params]
+		res = RestClient.get url
 		puts res
 	end
 
@@ -44,6 +46,7 @@ class Client
 end
 
 if __FILE__ == $0
-	c = Client.new(__APIS__['dpla'])
-	c.search('items', 'mice', '"moving image"')
+	c = Client.new(__APIS__[:dpla])
+	video = c.search('items', 'mice', '"moving image"')
+	audio = c.search('items', 'mice', 'sound')
 end
