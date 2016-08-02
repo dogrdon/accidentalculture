8348b2e23108aa0d77ef051eea867b863137044b
droquo <drew.s.gordon@gmail.com>
2016-08-01 22:49:42 -0400

    sub wasn't doing it properly

--- lib/accidentalculture/word.rb
+++ lib/accidentalculture/word.rb
@@ -10,7 +10,7 @@ module Word
   def self.get_word
   	searchword = RandomWord.adjs.next
     if searchword.include?('_')
-      searchword.sub('_', ' ')
+      searchword.sub!('_', ' ')
     end
     return searchword
   end
